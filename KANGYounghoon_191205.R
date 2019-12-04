d95fe2d928a6b9cf34a12582754218b89773e44e
CityHopper <peacestrokes@gmail.com>
2019-12-04 23:34:53 +0900

    191204

--- KANGYounghoon_191205.R
+++ KANGYounghoon_191205.R
@@ -14,8 +14,7 @@ na_count <- apply(ds,2,ds_na)
 na_count
 
 #(2) 결측값이 포함된 행들의 데이터를 출력하시오.
-na_countr <- apply(ds,1,ds_na)
-na_countr
+ds[ !complete.cases(ds),]
 
 #(3) 결측값이 포함된 행은 몇 개인지 출력하시오.
 sum(rowSums(is.na(ds))>0)
@@ -30,12 +29,15 @@ st <- data.frame(state.x77)
 #(1) st의 각 변수(열)들에 대해 특이값이 존재하는지 상자그림을 그려 확인하시오.
 boxplot(st)
 
-(2) 특이값이 존재하는 경우 이상치를 NA로 대체하여 저장하시오.
-out.val <- boxplot.stats(st$Area)$out; out.val
-st$Area[st$Area %in% out.val] <- NA
+#(2) 특이값이 존재하는 경우 이상치를 NA로 대체하여 저장하시오.
+for (i in 1:ncol(st)){
+  out.val <- boxplot.stats(st[,i])$out
+  st[,i][st[,i] %in% out.val] <- NA
+}
 
 #(3) st에서 NA가 존재하는 행들을 제거하여 st2에 저장하시오.
-st2 <- st[complete.cases(st),];
+st2 <- st[complete.cases(st),]
+
 
 #문3) R에서 제공하는 airquality 데이터셋에 대해 다음의 문제를 해결하기 위한 R 코드를 작성하시오.
 
@@ -44,41 +46,55 @@ AQ <- airquality
 AQ
 
 #(2) AQ에서 열별로 NA의 개수를 출력하시오.
-aqcount <- apply(AQ,2,ds_na); aqcount
+colSums(is.na(AQ))
 
 #(3) AQ에서 행별로 NA의 개수를 출력하시오.
-aqcount <- apply(AQ,1,ds_na); aqcount
+rowSums(is.na(AQ))
 
 #(4) AQ에서 NA를 포함하지 않은 행들만 출력하시오.
 nona <- AQ[complete.cases(AQ),]; nona
 
 #(5) AQ에서 NA를 NA가 속한 열의 평균값으로 치환하여 AQ2로 저장하고, AQ2의 내용을 출력하시오.
+AQ2 <- AQ;
+for (i in 1:ncol(AQ2)){
+    AQ2[,i][is.na(AQ2[,i])] <- mean(AQ2[,i],na.rm = TRUE)
+}
 AQ2
 
-문4)
-R에서 제공하는 state.x77 데이터셋에 대하여 다음 문제를 해결하기 위한
-R 코드를 작성하시오.
 
-(1) state.x77 데이터셋을 Population(인구수)를 기준으로 오름차순 정렬하시오.
-(2) state.x77 데이터셋을 Income(소득)을 기준으로 내림차순 정렬하시오.
-(3) Illiteracy(문맹률)가 낮은 상위 10개 주의 이름과 문맹률을 출력하시오.	
+#문4) R에서 제공하는 state.x77 데이터셋에 대하여 다음 문제를 해결하기 위한 R 코드를 작성하시오.
 
-문5)
-R에서 제공하는 mtcars 데이터셋에 대하여 다음 문제를 해결하기 위한 R
-코드를 작성하시오.
+#(1) state.x77 데이터셋을 Population(인구수)를 기준으로 오름차순 정렬하시오.
+library(dplyr)
+usa <- data.frame(state.x77)
+usa %>% arrange(Population)
 
-(1) mtcars 데이터셋을 gear(기어)의 개수에 따라 그룹을 나누어 mt.gear에 저장하
-시오.(단, split() 함수를 사용)
-(2) mt.gear에서 gear(기어)의 개수가 4인 그룹의 데이터를 출력하시오.
-(3) mt.gear에서 gear(기어)의 개수가 3인 그룹과 5인 그룹의 데이터를 합쳐서
-mt.gear.35에 저장하고 내용을 출력하시오.
-(4) mtcars 데이터셋에서 wt(중량)가 1.5~3.0 사이인 행들을 추출하여 출력하시오.
+#(2) state.x77 데이터셋을 Income(소득)을 기준으로 내림차순 정렬하시오.
+usa %>% arrange(desc(Income))
 
-문6)
-다음의 문제를 해결하기 위한 R코드를 작성하시오.
+#(3) Illiteracy(문맹률)가 낮은 상위 10개 주의 이름과 문맹률을 출력하시오.	
+rownames(head(usa[order(usa$Illiteracy),],10))
+head(usa[order(usa$Illiteracy),],10)[,"Illiteracy"]
 
-다음과 같이 데이터프레임 authors와 books를 생성하고 authors와 books의 내
-용을 출력하시오.	
+
+#문5)R에서 제공하는 mtcars 데이터셋에 대하여 다음 문제를 해결하기 위한 R코드를 작성하시오.
+
+#(1) mtcars 데이터셋을 gear(기어)의 개수에 따라 그룹을 나누어 mt.gear에 저장하시오.(단, split() 함수를 사용)
+mt.gear <- split(mtcars, mtcars$gear)
+
+#(2) mt.gear에서 gear(기어)의 개수가 4인 그룹의 데이터를 출력하시오.
+mt.gear$`4`
+
+#(3) mt.gear에서 gear(기어)의 개수가 3인 그룹과 5인 그룹의 데이터를 합쳐서 mt.gear.35에 저장하고 내용을 출력하시오.
+mt.gear.35 <- rbind(mt.gear$`3`, mt.gear$`5`)
+mt.gear.35
+
+#(4) mtcars 데이터셋에서 wt(중량)가 1.5~3.0 사이인 행들을 추출하여 출력하시오.
+mtcars %>% filter(mtcars$wt < 3.0 & mtcars$wt > 1.5)
+
+
+# 문6)다음의 문제를 해결하기 위한 R코드를 작성하시오.
+# 다음과 같이 데이터프레임 authors와 books를 생성하고 authors와 books의 내용을 출력하시오.	
 
 authors <- data.frame( surname = c( "Twein", "Venables", "Tierney", "Ripley", "McNeil" ),
                        nationality = c( "US", "Australia", "US", "UK", "Australia" ),
@@ -90,12 +106,25 @@ books <- data.frame( name = c( "Johns", "Venables", "Tierney", "Ripley", "Ripley
                                 "Spatial Statistics", "Stochastic Simulation",
                                 "Interactive Data Analysis" ),
                      other.author = c( NA, "Ripley", NA, NA, NA, NA ) )
+authors; 
+books
 
-(1) surname과 name을 공통 열로 하여 authors와 books를 병합하여 출력하시오(두						   
-                                                       데이터프레임에서 공통 열의 값이 일치하는 것들만 병합).
-(2) surname과 name을 공통 열로 하여 authors와 books를 병합하여 출력하되
-authors의 행들이 모두 표시되도록 하시오.
-(3) surname과 name을 공통 열로 하여 authors와 books를 병합하여 출력하되 books
-의 행들은 모두 표시되도록 하시오.
-(4) surname과 other.author를 공통 열로 하여 authors와 books를 병합하여 출력하
-시오.	
+#(1) surname과 name을 공통 열로 하여 authors와 books를 병합하여 출력하시오(두 데이터프레임에서 공통 열의 값이 일치하는 것들만 병합).
+merge(x=authors,y=books, 
+      by.x = c("surname"),
+      by.y = c("name"))
+
+#(2) surname과 name을 공통 열로 하여 authors와 books를 병합하여 출력하되 authors의 행들이 모두 표시되도록 하시오.
+merge(x=authors,y=books, all.x = T,
+      by.x = c("surname"),
+      by.y = c("name"))
+
+#(3) surname과 name을 공통 열로 하여 authors와 books를 병합하여 출력하되 books의 행들은 모두 표시되도록 하시오.
+merge(x=authors,y=books, all.y = T,
+      by.x = c("surname"),
+      by.y = c("name"))
+
+#(4) surname과 other.author를 공통 열로 하여 authors와 books를 병합하여 출력하시오.	
+merge(x=authors, y=books,
+      by.x = c("surname"),
+      by.y = c("other.author"))
